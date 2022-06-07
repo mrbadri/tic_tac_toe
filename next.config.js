53ae69a6500a015666ebad3f50d523d0eb78c5b2
mohammad.b <mohammad.b@arianalabs.io>
2022-06-07 18:47:09 +0430

    DOC: * add alias on next.config * change import on _app.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,21 @@
-/** @type {import('next').NextConfig} */
+/**
+ * @type {import('next').NextConfig}
+ * */
+
+const path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
-}
+  // custom webpack
+  webpack: (config) => {
+    config.resolve.alias = Object.assign(config.resolve.alias, {
+      "@/src": path.resolve(__dirname, "src"),
+      "@/public": path.resolve(__dirname, "public"),
+      "@/styles": path.resolve(__dirname, "styles"),
+      "@/utils": path.resolve(__dirname, "src/utils"),
+    });
 
-module.exports = nextConfig
+    return config;
+  },
+};
+module.exports = nextConfig;
